68d0d84bc75bb91ff40794f24027037a0db249c1
Hannah1563 <h.tuyishimi@alustudent.com>
2024-10-25 00:43:34 -0700

    3-print_alphabt.py

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 for i in range(97, 123):
-    if i != 101 and i != 113:  # Skip 'e' (101) and 'q' (113)
-        print(chr(i), end="")
+    if chr(i) not in ['q', 'e']:
+        print("{}".format(chr(i)), end="")
