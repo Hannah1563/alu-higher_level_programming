40c40cb37a9b5d0468fc54fed34e18540e2088be
Hannah1563 <h.tuyishimi@alustudent.com>
2024-11-06 00:23:59 -0800

    3-square.py

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Defines a Square class with a private size attribute, size validation, and area calculation."""
+"""Defines a Square class with a private size attribute, size validation, 
+and area calculation."""
+
 
 class Square:
     """Represents a square."""
