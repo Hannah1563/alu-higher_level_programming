d8364058798d083297b41cee2d6c5fe6ea846f6f
Hannah1563 <h.tuyishimi@alustudent.com>
2024-11-08 00:41:43 -0800

    3-square.py

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,31 +1,29 @@
 #!/usr/bin/python3
-"""Defines a Square class with a private size attribute, size validation, 
-and area calculation."""
+"""defines a class Square with size validation and area."""
 
 
 class Square:
     """Represents a square."""
 
     def __init__(self, size=0):
-        """Initializes a new Square instance.
-
+        """
+        Initializes a new square with size validation
         Args:
-            size (int): The size of the new Square. Defaults to 0.
-
+            size (int): The size of the square. Defaults to 0.
         Raises:
-            TypeError: If `size` is not an integer.
-            ValueError: If `size` is less than 0.
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
 
     def area(self):
-        """Calculates the current square area.
-
+        """
+        Returns the current square area.
         Returns:
-            int: The current square area.
+            int: The area of the square.
         """
         return self.__size ** 2
