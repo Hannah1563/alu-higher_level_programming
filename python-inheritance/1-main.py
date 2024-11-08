6737d53dcafbafad96cc5bfdc3beb00b5c83ff74
Hannah1563 <h.tuyishimi@alustudent.com>
2024-11-08 06:13:04 -0800

    8-rectangle.py

--- python-inheritance/1-main.py
+++ python-inheritance/1-main.py
@@ -1,18 +1,4 @@
-#!/usr/bin/python3
-MyList = __import__('1-my_list').MyList
-
-my_list = MyList()
-my_list.append(1)
-my_list.append(4)
-my_list.append(2)
-my_list.append(3)
-my_list.append(5)
-
-# Original list print
-print(my_list)  # [1, 4, 2, 3, 5]
-
-# Print sorted list (without modifying the original list)
-my_list.print_sorted()  # [1, 2, 3, 4, 5]
-
-# Original list is unchanged after print_sorted
-print(my_list)  # [1, 4, 2, 3, 5]
+class MyList(list):
+    def print_sorted(self):
+        """Prints the list in ascending order."""
+        print(sorted(self))
--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,22 +1,14 @@
-#!/usr/bin/python3
-"""
-Module containing the Rectangle class that inherits from BaseGeometry.
-"""
 from 7-base_geometry import BaseGeometry
 
 class Rectangle(BaseGeometry):
-    """
-    Class that defines a Rectangle, inheriting from BaseGeometry.
-    """
     def __init__(self, width, height):
-        """
-        Initializes a Rectangle instance.
-
-        Args:
-            width (int): The width of the rectangle.
-            height (int): The height of the rectangle.
-        """
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
         self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
