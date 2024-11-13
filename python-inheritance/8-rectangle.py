cde1c01b161872ed8dbecd0583bf41d278fa7d51
Hannah1563 <h.tuyishimi@alustudent.com>
2024-11-13 00:37:26 -0800

    8-rectangle.py

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,14 +1,14 @@
-from 7-base_geometry import BaseGeometry
+#!/usr/bin/python3
+"""Rectangle class"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
 
 class Rectangle(BaseGeometry):
+    """Rectangle class"""
+
     def __init__(self, width, height):
-        self.integer_validator("width", width)
+        """Initializes a Rectangle object"""
+        super().integer_validator("width", width)
         self.__width = width
-        self.integer_validator("height", height)
+        super().integer_validator("height", height)
         self.__height = height
-
-    def area(self):
-        return self.__width * self.__height
-
-    def __str__(self):
-        return f"[Rectangle] {self.__width}/{self.__height}"
