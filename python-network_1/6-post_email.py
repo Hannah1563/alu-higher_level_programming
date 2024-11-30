afdd0aa2d911c208c5b0b19f1fddad9d1708cb2e
Hannah1563 <h.tuyishimi@alustudent.com>
2024-11-30 14:42:25 +0000

    6-post_email.py

--- python-network_1/6-post_email.py
+++ python-network_1/6-post_email.py
@@ -2,16 +2,32 @@
 import sys
 import requests
 
-# Get the URL and email from the command-line arguments
-url = sys.argv[1]
-email = sys.argv[2]
+"""
+A script that sends a POST request to a specified URL with an email as a parameter.
 
-# Create a dictionary with the 'email' parameter
-data = {'email': email}
+Usage:
+    ./6-post_email.py <URL> <email>
 
-# Send a POST request to the URL with the email parameter
-response = requests.post(url, data=data)
+Args:
+    URL: The URL where the POST request should be sent.
+    email: The email address to be sent as a parameter in the POST request.
+"""
 
-# Print the response body
-print(response.text)
+def main():
+    # Get the URL and email from the command-line arguments
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    # Create a dictionary with the 'email' parameter
+    data = {'email': email}
+
+    # Send a POST request to the URL with the email parameter
+    response = requests.post(url, data=data)
+
+    # Print the response body
+    print(response.text)
+
+
+if __name__ == "__main__":
+    main()
 
