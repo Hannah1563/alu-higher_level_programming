62a7f4a5b8017c0c062548cd78c56cdc0582f180
Hannah1563 <h.tuyishimi@alustudent.com>
2024-12-01 11:45:12 +0000

    10-my_github.py

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -1,40 +1,21 @@
 #!/usr/bin/python3
-import sys
+"""
+Python script that takes GitHub credentials (username and password)
+and uses the GitHub API to display your id
+Username and token are given as arguments
+"""
 import requests
+import sys
 
-"""
-A script that uses the GitHub API with Basic Authentication to fetch and display
-the user ID for a specified GitHub username and personal access token.
 
-Usage:
-    ./10-my_github.py <username> <personal_access_token>
-
-Arguments:
-    username: GitHub username.
-    personal_access_token: GitHub personal access token with 'read:user' permission.
-
-Output:
-    The user ID associated with the provided GitHub username.
-    If authentication fails (wrong username or token), None is printed.
-"""
-
-def main():
-    # Get GitHub username and personal access token (password) from command line arguments
+if __name__ == "__main__":
     username = sys.argv[1]
     token = sys.argv[2]
 
-    # GitHub API URL to get user information
     url = "https://api.github.com/user"
-
-    # Send GET request with Basic Authentication
     response = requests.get(url, auth=(username, token))
 
-    # If the authentication is successful, print the user ID, otherwise print None
     if response.status_code == 200:
         print(response.json().get('id'))
     else:
         print(None)
-
-if __name__ == "__main__":
-    main()
-
